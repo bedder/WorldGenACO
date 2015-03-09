0a427939f1da464da6dd9e30b0a54d41df79c002
Matthew Bedder <bedder@gmail.com>
2015-03-09 12:41:10 +0000

    Added pretty circling camera

---
+++ Assets/CirclingCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CirclingCamera : MonoBehaviour {
+    // CirclingCamera Dynamics Variables
+    public float rotationRate = 5f;
+
+    void Update() {
+        gameObject.transform.Rotate(Vector3.up, rotationRate * Time.deltaTime);
+    }
+}
