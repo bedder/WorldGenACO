2855cf6d3dc52417b2c6e8ae8c96eb0b9adf6de1
Matthew Bedder <bedder@gmail.com>
2015-03-11 11:42:48 +0000

    Added updating bounds for Visualisation controller

--- Assets/Scripts/Visualisation/VisualisationController.cs
+++ Assets/Scripts/Visualisation/VisualisationController.cs
@@ -16,11 +16,13 @@ public class VisualisationController : MonoBehaviour {
     // Bounds
     public float maxHeight = 10f;
     public float maxPheremone = 10f;
-    public float maxVisits = 10f;
+    public int maxVisits = 10;
+    public float boundsUpdateFrequency = 5f;
+    private float nextBoundsUpdate = 0f;
 
     // Visualisation colours
     private Color[] pheremoneColour = { new Color(1f, 1f, 1f, 1f), new Color(1f, 0f, 1f, 1f) };
-    private Color[] waterColour = { new Color(1f, 0f, 0f, 1f), new Color(0f, 0f, 1f, 1f) };
+    private Color[] waterColour = { new Color(1f, 0f, 1f, 1f), new Color(0f, 0f, 1f, 1f) };
     private Color[] heightColour = { new Color(1f, 1f, 0f, 1f), new Color(1f, 0f, 0f, 1f) };
     private Color[] footfall = { new Color(0f, 0f, 0f, 1f), new Color(1f, 1f, 1f, 1f) };
 
@@ -33,7 +35,7 @@ public class VisualisationController : MonoBehaviour {
             if (tile != null) {
                 switch (mode) {
                     case VisualisationMode.footfall:
-                        tile.setVisualisationColor(interpolateColour(footfall, tile.getVisits() / maxVisits));
+                        tile.setVisualisationColor(interpolateColour(footfall, (float)tile.getVisits() / maxVisits));
                        break;
                     case VisualisationMode.height:
                         tile.setVisualisationColor(interpolateColour(heightColour, tile.height / maxHeight));
@@ -51,6 +53,12 @@ public class VisualisationController : MonoBehaviour {
             }
         }
     }
+    private void updateBounds() {
+        foreach (HexTile tile in FindObjectsOfType<HexTile>())
+            if (!tile.isFoodSource() && !tile.isNest())
+                maxVisits = Mathf.Max(maxVisits, tile.getVisits());
+        nextBoundsUpdate = Time.realtimeSinceStartup + boundsUpdateFrequency;
+    }
 
     // Unity logic functions
     void Awake() {
@@ -60,6 +68,8 @@ public class VisualisationController : MonoBehaviour {
         tiles = GameObject.FindObjectsOfType<HexTile>();
     }
     void Update() {
+        if (nextBoundsUpdate < Time.realtimeSinceStartup)
+            updateBounds();
         if (Input.GetButtonDown("VisualisationFootfall")) {
             mode = (mode == VisualisationMode.footfall ? VisualisationMode.none : VisualisationMode.footfall);
         } else if (Input.GetButtonDown("VisualisationHeight")) {
