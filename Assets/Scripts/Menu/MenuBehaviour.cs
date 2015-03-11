787ec8f2e68924c034bddc35ede2526af3e0509c
Matthew Bedder <bedder@gmail.com>
2015-03-11 22:51:03 +0000

    Added (largely functioning) main menu

---
+++ Assets/Scripts/Menu/MenuBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBehaviour : MonoBehaviour {
+    public void startLevel() {
+        Application.LoadLevel(1);
+    }
+
+    public void startCustomLevel() {
+        Debug.Log("This is not yet implemented.");
+    }
+
+    public void openIggiWebsite() {
+        System.Diagnostics.Process.Start("http://iggi.org.uk");
+    }
+
+    public void openYccsaWebsite() {
+        System.Diagnostics.Process.Start("https://www.york.ac.uk/yccsa/");
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
+}
