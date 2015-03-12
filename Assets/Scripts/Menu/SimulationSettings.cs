c6c9c3fdfa41d81a36c1f8e58bcc07476cb869a4
Matthew Bedder <bedder@gmail.com>
2015-03-12 14:13:55 +0000

    Improved custom simulation UI

---
+++ Assets/Scripts/Menu/SimulationSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimulationSettings : MonoBehaviour {
+    // Map params
+    public int nNests = 5;
+    public int nFoodSources = 20;
+    public float heightFactor = 10f;
+    public float perlinFactor = 0.03f;
+
+    // Food source
+    public int restockFrequency = 100;
+    public float restockFactor = 1.5f;
+
+    // Nest
+    public float initialFood = 300f;
+    public float foodPerAnt = 0.1f;
+    public int maxAnts = 1;
+    public int antSpawnFrequency = 5;
+
+    // Ant
+    public float pheremoneRelease = 100f;
+    public float pheremoneAttraction = 1f;
+    public float pheremoneFollowing = 2f;
+    public float terrainFollowing = 1f;
+}
---
+++ Assets/Scripts/Menu/SliderUpdater.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderUpdater : MonoBehaviour {
+    public string format = "0";
+
+    public void setValue(float value) {
+        GetComponent<Text>().text = value.ToString(format);
+    }
+}
